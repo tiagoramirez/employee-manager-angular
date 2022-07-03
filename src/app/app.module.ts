342cd3539f6fb748908b8a9776fa6a733c310f98
Tiago Ramirez <tiagoramirez2001@gmail.com>
2022-07-03 01:55:08 -0300

    Configured styles, tested colors and defined the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+    { path: '', component: EmpleadosComponent },
+    { path: 'aboutMe', component: AboutMeComponent }
+]
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        EmpleadosComponent,
+        AboutMeComponent,
+        NavBarComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
